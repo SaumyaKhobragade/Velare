48f2b515017753b79d88958855f89512afd2f389
Saumya Khobragade <khobragadesaumya@gmail.com>
2026-05-27 16:16:50 +0530

    Add Listing Model

---
+++ models/listing.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    location: String,
+    country: String
+});
+
+const Listing = mongoose.model('Listing', listingSchema);
