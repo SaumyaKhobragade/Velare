42415e86d72e5ce3d025a88d67b4903786e14b3c
Saumya Khobragade <khobragadesaumya@gmail.com>
2026-06-25 23:51:05 +0530

    Restructure Reviews Using Express Router

--- app.js
+++ app.js
@@ -4,16 +4,10 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import methodOverride from 'method-override';
 import ejsMate from 'ejs-mate';
-import joi from 'joi';
 
-import Listing from './models/listing.js';
-import wrapAsync from './utils/wrapAsync.js';
 import ExpressError from './utils/ExpressError.js';
-import listingSchema from './schema.js';
-import Review from './models/review.js';
-import { reviewSchema } from './schema.js';
-
 import listingRoutes from './routes/listing.js';
+import reviewRoutes from './routes/review.js';
 
 const app = express();
 const port = 3000;
@@ -40,37 +34,8 @@ app.get('/', (req, res) => {
     res.send('Working');
 });
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(', ');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-};
-
 app.use('/listings', listingRoutes);
-
-// Reviews
-// Post Route for Reviews
-app.post('/listings/:id/reviews', validateReview, wrapAsync(async (req, res) => {
-    const listing = await Listing.findById(req.params.id);
-    const review = new Review(req.body.review);
-    listing.reviews.push(review);
-    await review.save();
-    await listing.save();
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-// Delete Route for Reviews
-app.delete('/listings/:id/reviews/:reviewId', wrapAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    const listing = await Listing.findById(id);
-    res.redirect(`/listings/${listing._id}`);
-}));
+app.use('/listings/:id/reviews', reviewRoutes);
 
 app.use((req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
@@ -78,7 +43,6 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = 'Something went wrong' } = err;
-    // res.status(statusCode).send(message);
     res.render('error', { statusCode, message });
 });
 
---
+++ routes/review.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import wrapAsync from '../utils/wrapAsync.js';
+import ExpressError from '../utils/ExpressError.js';
+import Review from '../models/review.js';
+import { reviewSchema } from '../schema.js';
+import Listing from '../models/listing.js';
+
+const router = express.Router({ mergeParams: true });
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(', ');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+};
+
+// Post Route for Reviews
+router.post('/', validateReview, wrapAsync(async (req, res) => {
+    const listing = await Listing.findById(req.params.id);
+    const review = new Review(req.body.review);
+    listing.reviews.push(review);
+    await review.save();
+    await listing.save();
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+// Delete Route for Reviews
+router.delete('/:reviewId', wrapAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    const listing = await Listing.findById(id);
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+export default router;
