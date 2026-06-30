c8de9af9b88b61577ade0a5d3bf4115c14dcdf1e
Saumya Khobragade <khobragadesaumya@gmail.com>
2026-06-30 20:23:50 +0530

    Add SignUp Functionality

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import User from '../models/user.js';
+import wrapAsync from '../utils/wrapAsync.js';
 
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
     res.render('users/signup');
 });
+
+router.post('/signup', wrapAsync(async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = new User({ username, email });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Velare!');
+        res.redirect('/listings');
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/signup');
+    }
+}));
 
 export default router;
