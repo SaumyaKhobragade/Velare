b69b025e8ab187ccd47e0e4da4af28d377007f64
Saumya Khobragade <khobragadesaumya@gmail.com>
2026-07-25 00:29:11 +0530

    Store Coordinates and Use Them

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,65 +1,83 @@
-import Listing from '../models/listing.js';
-import wrapAsync from '../utils/wrapAsync.js';
+import Listing from "../models/listing.js";
+import wrapAsync from "../utils/wrapAsync.js";
+import mbxGeocoding from "@mapbox/mapbox-sdk/services/geocoding.js";
+
+const mapToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapToken });
 
 export const index = wrapAsync(async (req, res) => {
     const listings = await Listing.find({});
-    res.render('listings/index', { listings });
+    res.render("listings/index", { listings });
 });
 
 export const renderNewForm = (req, res) => {
-    res.render('listings/new');
+    res.render("listings/new");
 };
 
 export const showListing = wrapAsync(async (req, res) => {
-    const listing = await Listing.findById(req.params.id).populate({ path: 'reviews', populate: { path: 'author' } }).populate('owner');
+    const listing = await Listing.findById(req.params.id)
+        .populate({ path: "reviews", populate: { path: "author" } })
+        .populate("owner");
     if (!listing) {
-        req.flash('error', 'Cannot find that listing!');
-        return res.redirect('/listings');
+        req.flash("error", "Cannot find that listing!");
+        return res.redirect("/listings");
     }
-    res.render('listings/show', { listing });
+    res.render("listings/show", { listing });
 });
 
 export const createListing = wrapAsync(async (req, res, next) => {
+    let response = await geocoder
+        .forwardGeocode({
+            query: req.body.listing.location + ", " + req.body.listing.country,
+            limit: 1,
+        })
+        .send()
+
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
     newListing.image = {
         url: req.file.path,
-        filename: req.file.filename
+        filename: req.file.filename,
     };
+    newListing.geometry = response.body.features[0].geometry;
     await newListing.save();
-    req.flash('success', 'Successfully made a new listing!');
-    res.redirect('/listings');
+    req.flash("success", "Successfully made a new listing!");
+    res.redirect("/listings");
 });
 
 export const renderEditForm = wrapAsync(async (req, res) => {
     const listing = await Listing.findById(req.params.id);
     if (!listing) {
-        req.flash('error', 'Cannot find that listing!');
-        return res.redirect('/listings');
+        req.flash("error", "Cannot find that listing!");
+        return res.redirect("/listings");
     }
 
     let originalImageUrl = listing.image.url;
-    originalImageUrl = originalImageUrl.replace('/upload', '/upload/w_250');
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
 
-    res.render('listings/edit', { listing, originalImageUrl });
+    res.render("listings/edit", { listing, originalImageUrl });
 });
 
 export const updateListing = wrapAsync(async (req, res) => {
-    const finalListing = await Listing.findByIdAndUpdate(req.params.id, req.body.listing, { returnDocument: 'after' });
+    const finalListing = await Listing.findByIdAndUpdate(
+        req.params.id,
+        req.body.listing,
+        { returnDocument: "after" },
+    );
 
-    if (typeof req.file !== 'undefined') {
+    if (typeof req.file !== "undefined") {
         finalListing.image = {
             url: req.file.path,
-            filename: req.file.filename
+            filename: req.file.filename,
         };
         await finalListing.save();
     }
-    req.flash('success', 'Successfully updated listing!');
+    req.flash("success", "Successfully updated listing!");
     res.redirect(`/listings/${finalListing._id}`);
 });
 
 export const destroyListing = wrapAsync(async (req, res) => {
     const listing = await Listing.findByIdAndDelete(req.params.id);
-    req.flash('success', 'Successfully deleted listing!');
-    res.redirect('/listings');
+    req.flash("success", "Successfully deleted listing!");
+    res.redirect("/listings");
 });
--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,17 @@
 mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [77.2090, 28.6139], // [longitude, latitude] opposite of normal coordinates
-    zoom: 12
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: listing.geometry.coordinates, // [longitude, latitude] opposite of normal coordinates
+    zoom: 12,
 });
+
+const marker = new mapboxgl.Marker({ color: "red" })
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `<h4>${listing.title}</h4><p>Exact location will be provided after booking.</p>`,
+        ),
+    )
+    .addTo(map);
